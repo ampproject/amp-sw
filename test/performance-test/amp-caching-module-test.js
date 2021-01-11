b9f85c706586009a9161f869868eba8f2fdd972c
Kristofer Baxter <kristofer@kristoferbaxter.com>
2021-01-11 11:27:22 -0800

    Updated dependencies

--- test/performance-test/amp-caching-module-test.js
+++ test/performance-test/amp-caching-module-test.js
@@ -28,7 +28,7 @@ const serviceWorkerPath = join('test', 'amp-caching-sw.js');
 const writeFile = promisify(fs.writeFile);
 
 async function sleep(interval) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     setTimeout(resolve, interval);
   });
 }
@@ -36,7 +36,7 @@ async function sleep(interval) {
 async function interceptAmpScripts(page) {
   await page.setRequestInterception(true);
   // intercept js requests to cdn.ampproject.org and change cache headers for only 15 seconds;
-  page.on('request', async request => {
+  page.on('request', async (request) => {
     if (/https:\/\/cdn.ampproject.org\/.*\.js/.test(request.url())) {
       const response = await fetch(request.url());
       const body = await response.text();
@@ -66,50 +66,50 @@ test.after(() => {
   server.close();
 });
 
-test('should enable fast script response when the browser cache fails to respond', async t => {
-  let browser = await puppeteer.launch();
-  let page = await browser.newPage();
+async function performanceOfScripts(useServiceWorker) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
   await interceptAmpScripts(page);
-  // load page for the first time
-  await page.goto('http://localhost:6881/test/accordian.amp.html', {
-    waitUntil: 'networkidle0',
-  });
-  // let cache expire
-  await sleep(4000);
-  // reload the page
-  await page.reload({
-    waitUntil: 'networkidle0',
-  });
-  const totalAmpScriptsLoadTimeWithoutSW = await page.evaluate(() => {
+
+  try {
+    // load page for the first time
+    await page.goto('http://localhost:6881/test/accordian.amp.html', {
+      waitUntil: 'networkidle0',
+    });
+    console.log('baseline retrieval success');
+
+    if (useServiceWorker) {
+      await page.setOfflineMode(true);
+      await page.evaluate(() =>
+        navigator.serviceWorker.register('/test/amp-caching-sw.js'),
+      );
+    }
+
+    // reload the page
+    await page.reload({
+      waitUntil: 'networkidle0',
+    });
+    console.log('comparison retrieval success');
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+
+  const evalutedTime = await page.evaluate(() => {
     return performance
       .getEntriesByType('resource')
-      .filter(resource => resource.initiatorType === 'script')
+      .filter((resource) => resource.initiatorType === 'script')
       .reduce((current, next) => current + next.duration, 0);
   });
   browser.close();
-  browser = await puppeteer.launch();
-  page = await browser.newPage();
-  await interceptAmpScripts(page);
-  // load tpage the first time
-  await page.goto('http://localhost:6881/test/accordian.amp.html', {
-    waitUntil: 'networkidle0',
-  });
-  await page.setOfflineMode(true);
-  await page.evaluate(() =>
-    navigator.serviceWorker.register('/test/amp-caching-sw.js'),
-  );
-  // let cache expire
-  await sleep(4000);
-  // reload the page
-  await page.reload({
-    waitUntil: 'networkidle0',
-  });
-  const totalAmpScriptsLoadTimeWithSW = await page.evaluate(() => {
-    return performance
-      .getEntriesByType('resource')
-      .filter(resource => resource.initiatorType === 'script')
-      .reduce((current, next) => current + next.duration, 0);
-  });
+
+  return evalutedTime;
+}
+
+test('should enable fast script response when the browser cache fails to respond', async (t) => {
+  const totalAmpScriptsLoadTimeWithoutSW = await performanceOfScripts(false);
+  const totalAmpScriptsLoadTimeWithSW = await performanceOfScripts(true);
+
   t.assert(
     totalAmpScriptsLoadTimeWithSW < 0.3 * totalAmpScriptsLoadTimeWithoutSW,
   );
