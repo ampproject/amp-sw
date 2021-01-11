cdabba4717c94972029cffdfcfddf67f8f1565c6
Kristofer Baxter <kristofer@kristoferbaxter.com>
2021-01-11 12:04:49 -0800

    Move timeout to ava cli

--- test/conformance-test/version-test.js
+++ test/conformance-test/version-test.js
@@ -38,7 +38,6 @@ test.after(async () => {
 });
 
 test('should have the package version for the bundle', async (t) => {
-  t.timeout(240000); // 4 minute timeout for CI.
   const entryBundlePath = resolve(__dirname, '../../dist/amp-sw.js');
   const entryBundleContent = await readFile(entryBundlePath, 'utf-8');
   t.assert(entryBundleContent.startsWith(`/*! AMP_SW_v${version} */`));
